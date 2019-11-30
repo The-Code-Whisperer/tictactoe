360ed265bacb3b0e6b465aae2a431c52147082ea
Steven Kim <hansomekim@gmail.com>
2019-11-30 00:57:21 -0500

    added AI

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
 from tempfile import mkdtemp
+from math import inf
 
 app = Flask(__name__)
 
@@ -13,14 +14,24 @@ Session(app)
 @app.route("/")
 def index():
     if "board" not in session:
-        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        session["board"] = [[None, None, None], [
+            None, None, None], [None, None, None]]
         session["turn"] = "X"
+    winner, gameOver = check_game_winner()
+    return render_template("game.html", game=session["board"], turn=session["turn"],
+                           winner=winner, gameOver=gameOver)
+
+
+def check_game_winner():
     winner = None
+    gameOver = False
+    # check rows
     for row in session["board"]:
         if set(row) == set("X"):
             winner = "X"
         if set(row) == set("O"):
             winner = "O"
+    # check columns
     for i in range(3):
         col_lst = []
         for row in session["board"]:
@@ -29,6 +40,7 @@ def index():
             winner = "X"
         if set(col_lst) == set("O"):
             winner = "O"
+    # check diagonals
     if session["board"][0][0] == session["board"][1][1] == session["board"][2][2] == "X":
         winner = "X"
     if session["board"][0][0] == session["board"][1][1] == session["board"][2][2] == "O":
@@ -37,7 +49,124 @@ def index():
         winner = "X"
     if session["board"][0][2] == session["board"][1][1] == session["board"][2][0] == "O":
         winner = "O"
-    return render_template("game.html", game=session["board"], turn=session["turn"], winner=winner)
+    # check if board is filled up (tied)
+    if winner or None not in session["board"][0] and None not in session["board"][1] and \
+            None not in session["board"][2]:
+        gameOver = True
+    return winner, gameOver
+
+
+@app.route("/aiMove")
+def aiMove():
+    # recursive max or min value finding function based on possible moves.
+    def minimax(board, turn):
+        # return a value if the hypothetical game is over.
+        winner, gameOver = check_game_winner()
+        if winner == "X":
+            return 1
+        if winner == "O":
+            return -1
+        if gameOver:
+            return 0
+        # if the game is still going, simulate best moves for current turn.
+        # make a list of tuples representing possible moves.
+        moves = []
+        for row in range(3):
+            for col in range(3):
+                if not board[row][col]:
+                    moves.append((row, col))
+        if turn == "X":
+            value = -inf
+            for move in moves:
+                row, col = move
+                # hypothesize a board with that move
+                board[row][col] = turn
+                # check how good the new board is
+                value = max(value, minimax(board, "O"))
+                # put the board back to its original state to check the next move
+                board[row][col] = None
+                # which of these moves produced the highest score?
+        else:
+            value = inf
+            for move in moves:
+                row, col = move
+                # hypothesize a board with that move
+                board[row][col] = turn
+                # check how good the new board is
+                value = min(value, minimax(board, "X"))
+                # put the board back to its original state to check the next move
+                board[row][col] = None
+                # which of these moves produced the lowest score?
+        return value
+    # find the row and col of the best move. Need to find the coordinates of the first move that
+    # gives best value.
+    board = session["board"]
+    turn = session["turn"]
+    # make a list of tuples representing possible moves.
+    moves = []
+    for row in range(3):
+        for col in range(3):
+            if not board[row][col]:
+                moves.append((row, col))
+    # iterate over each move. Find each move's minimax.
+    for move in moves:
+        if turn == "X":
+            value = -inf
+            for move in moves:
+                row, col = move
+                # hypothesize a board with that move
+                board[row][col] = turn
+                # check how good the new board is
+                value = max(value, minimax(board, "O"))
+                # put the board back to its original state to check the next move
+                board[row][col] = None
+                # return first available best move.
+                if value == 1:
+                    return redirect(url_for("play", row=row, col=col))
+            # if there is never a best move, return a 2nd best (tie 0)
+            value = -inf
+            for move in moves:
+                row, col = move
+                # hypothesize a board with that move
+                board[row][col] = turn
+                # check how good the new board is
+                value = max(value, minimax(board, "O"))
+                # put the board back to its original state to check the next move
+                board[row][col] = None
+                # return first available 2nd best move.
+                if value == 0:
+                    return redirect(url_for("play", row=row, col=col))
+            # otherwise just return row and col for whatever the last one was.
+            return redirect(url_for("play", row=row, col=col))
+        else:
+            value = inf
+            for move in moves:
+                row, col = move
+                # hypothesize a board with that move
+                board[row][col] = turn
+                # check how good the new board is
+                value = min(value, minimax(board, "X"))
+                # put the board back to its original state to check the next move
+                board[row][col] = None
+                # return first available best move.
+                if value == -1:
+                    return redirect(url_for("play", row=row, col=col))
+            # if there is never a best move, return a 2nd best (tie 0)
+            value = inf
+            for move in moves:
+                row, col = move
+                # hypothesize a board with that move
+                board[row][col] = turn
+                # check how good the new board is
+                value = max(value, minimax(board, "X"))
+                # put the board back to its original state to check the next move
+                board[row][col] = None
+                # return first available 2nd best move.
+                if value == 0:
+                    return redirect(url_for("play", row=row, col=col))
+            # otherwise just return row and col for whatever the last one was.
+            return redirect(url_for("play", row=row, col=col))
+    # should have been redirected within the for loop. if not, just crash.
 
 
 @app.route("/play/<int:row>/<int:col>")
