59456c5693508bd0551aa290b75a1f3d61daac4a
Steven Kim <hansomekim@gmail.com>
2019-11-30 01:19:23 -0500

    short key

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["SECRET_KEY"] = "08wytnowyr84a9nwyrobseyorfarbo74tbsilc!@#$^%&*(dfbzisgaoweyusfzodgfca8w3irypyscndpsz"
+app.config["SECRET_KEY"] = "08wytnowyr84a9nwyrobseyorfarbo74tbsilco"
 Session(app)
 
 
