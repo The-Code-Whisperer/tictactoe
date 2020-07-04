f5894292cfbc9c4357948d4d2a920a1aae91a2c2
Steven Kim <97stevenkim@gmail.com>
2020-07-04 18:35:37 -0400

    session permanent true

--- application.py
+++ application.py
@@ -7,9 +7,9 @@ from random import randint
 app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
-app.config['SECRET_KEY'] = '08wytnowyr84a9nwyrobseyorfarbo74tbsilco'
+#app.config['SECRET_KEY'] = '08wytnowyr84a9nwyrobseyorfarbo74tbsilco'
 Session(app)
 
 
